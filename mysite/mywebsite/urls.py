63df9b063ac1634d5ff8aaf29d22690aadb1478b
Eoin Francis <eoin.francis3@mail.dcu.ie>
2024-01-25 22:03:51 +0000

    Added an empty interests page that can be accessed through the interests link at the top of the home page. A brief summary has been added to the home page below the interests link. Distinct borders have also been added for the h1 and h2 elements.

--- mysite/mywebsite/urls.py
+++ mysite/mywebsite/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('interests/', views.interests, name='interests')
 ]
--- mysite/mywebsite/views.py
+++ mysite/mywebsite/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
+def interests(request):
+    return render(request, 'interests.html')
+
 # Create your views here.
